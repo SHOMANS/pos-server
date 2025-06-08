4d91def0f938603d342cb1eb7264b4de485ae1e2
SHOMANS <mohammedshoman9@gmail.com>
2025-06-08 16:27:35 +0200

    Add CORS support to the application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import productsRouter from './product';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/products', productsRouter);
